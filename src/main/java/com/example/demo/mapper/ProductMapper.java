9073c159433c8cc9b1ed199ed249d140af6a1b6d
luke <42775462+luke@users.noreply.github.com>
2022-06-22 11:12:20 +0800

    加入呼叫API

--- src/main/java/com/example/demo/mapper/ProductMapper.java
+++ src/main/java/com/example/demo/mapper/ProductMapper.java
@@ -1,26 +1,26 @@
 package com.example.demo.mapper;
 
 import java.util.List;
-import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.pojo.Product;
 
-@Mapper  //這個註解表示了這個類是一個mybatis的mapper介面類
+@Mapper // 這個註解表示了這個類是一個mybatis的mapper介面類
 @Repository
 public interface ProductMapper {
-	
+	@Transactional
 	int insertProduct(String name, String location, String descript, int price, String unit);
 
 	int Product(Integer id);
-	
+
 	int deleteProduct(Integer id);
 
 	int updateProduct(Product product);
 
+	@Transactional
 	int updateProductByMap(String name, String location, String descript, int price, String unit, int id);
 
 	Product queryProductByID(int id);
